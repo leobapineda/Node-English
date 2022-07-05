8dcc369b7143389b574d795c17c8664cccd003df
Leobardo Pineda <leobapineda@gmail.com>
2022-07-05 10:09:15 -0500

    Feat: add individual task edit hmtl page

--- 03-task-manager/public/browser-app.js
+++ 03-task-manager/public/browser-app.js
@@ -14,14 +14,28 @@ const showTasks = async () => {
     if (tasks.length === 0) {
       tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>';
       loadingDOM.style.visibility = "hidden";
-      return
+      return;
     }
     const allTasks = tasks.map((task) => {
-        const {name, _id:taskID, completed} = task
-        return `<div class="indiviaul-task ${completed && "task-completed"}" >${name}</div>`
-    })
-    tasksDOM.innerHTML = allTasks
-    
+      const { name, _id: taskID, completed } = task;
+      // return `<span class="individual-task ${
+      //   completed && "task-completed"
+      // }" >${name}</span>
+      //  <div class="task-links" >
+      //   <div><a href="./" >edit</a></div>
+      //   <button>delete</button>
+      //  </div>
+      //   `;
+
+      return `<div class="inner-task-container">
+      <div class="individual-task ${completed && "task-completed"}" >${name}</div>
+      <div class="task-links">
+        <div><a href="./task.html?id=${taskID}">edit</a></div>
+        <button>delete</button>
+      </div>
+    </div>`
+    });
+    tasksDOM.innerHTML = allTasks;
   } catch (error) {
     console.log(error);
   }
