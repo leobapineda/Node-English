15575d4858222192f2342023bfbaf01eac5b6df9
Leobardo Pineda <leobapineda@gmail.com>
2022-07-06 21:06:27 -0500

    Feat: add regex to get products without giving exact name

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -2,12 +2,32 @@ const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res, next) => {
   // testing
-  const products = await Product.find({company: "caressa"});
-  res.status(200).json({ hits:products.length , products});
+  const searchText = "k"
+  const products = await Product.find({ name: { $regex: searchText /*the value we search in company name*/, $options: 'i' /*flag, in this case, means not sensitive*/} });
+  // const products = await Product.find({ });
+  res.status(200).json({ hits: products.length, products });
+
+
+
+
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "getAllProducts" });
+  const { featured, company, name, order } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ hits: products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
