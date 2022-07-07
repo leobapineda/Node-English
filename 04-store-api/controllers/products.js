19d8a591b8791b06cd6d6a42ae6e70de8e45e163
Leobardo Pineda <leobapineda@gmail.com>
2022-07-07 15:40:15 -0500

    Feat: add selection of products with range of price

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -2,18 +2,21 @@ const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res, next) => {
   // testing
-  const searchText = "k"
-  const products = await Product.find({ name: { $regex: searchText /*the value we search in company name*/, $options: 'i' /*flag, in this case, means not sensitive*/} });
+  const searchText = "k";
+  const products = await Product.find({
+    name: {
+      $regex: searchText /*the value we search in company name*/,
+      $options: "i" /*flag, in this case, means not sensitive*/,
+    },
+  });
   // const products = await Product.find({ });
   res.status(200).json({ hits: products.length, products });
-
-
-
-
 };
 
+// ------------>>>>>>>>>>>> getAllProducts
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, order } = req.query;
+  const { featured, company, name, sort, field, maxPrice, minPrice } =
+    req.query;
   const queryObject = {};
 
   if (featured) {
@@ -24,9 +27,40 @@ const getAllProducts = async (req, res) => {
     queryObject.company = company;
   }
   if (name) {
-    queryObject.name = { $regex: name, $options: 'i' };
+    queryObject.name = { $regex: name, $options: "i" };
   }
-  const products = await Product.find(queryObject);
+
+  // filter: between min and max values
+  if (maxPrice && minPrice) {
+    queryObject.price = { $gte: Number(minPrice), $lte: Number(maxPrice) };
+  } else if (maxPrice) {
+    queryObject.price = { $lte: Number(maxPrice) };
+  } else if (minPrice) {
+    queryObject.price = { $gte: Number(minPrice) };
+  }
+
+  let sortValue;
+  if (sort) {
+    sortValue = sort.split(",").join(" ");
+  } else {
+    sortValue = "price";
+  }
+  let fieldValue;
+  if (field) {
+    fieldValue = field.split(",").join(" ");
+  }
+
+  let limit = Number(req.query.limit) || 7;
+  const page = Number(req.query.page) || 1;
+  const skip = (page - 1) * limit;
+
+  const products = await Product.find(queryObject)
+    .sort(sortValue)
+    .select(fieldValue);
+  // .limit(limit)
+  // .skip(skip);
+
+  console.log(queryObject);
   res.status(200).json({ hits: products.length, products });
 };
 
