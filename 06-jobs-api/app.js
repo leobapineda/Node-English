af7e89f3c615e62148179f7bc811e7a89457ae89
Leobardo Pineda <leobapineda@gmail.com>
2022-07-14 12:09:17 -0500

    Feat: add extra security packages

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -1,18 +1,35 @@
 require("dotenv").config();
 require("express-async-errors");
+const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
 const jobRouter = require("./routes/jobs");
 const authRouter = require("./routes/auth");
-const authenticationMiddleware = require("./middleware/authentication");
 
+// extra security packages
+const helmet = require("helmet");
+var cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
+const authenticationMiddleware = require("./middleware/authentication");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
@@ -20,8 +37,6 @@ app.use("/api/v1/jobs", authenticationMiddleware, jobRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
-
-const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
